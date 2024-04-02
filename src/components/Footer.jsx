da15b1fd72ad2da33ef736cd6489b1232cd69ed8
mrsharifull <119003578+firoj10@users.noreply.github.com>
2024-04-02 17:22:46 +0600

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="shadow mx-auto py-5" >
       <div className='container'>
       <div className="row footers justify-content-between text-center">
-        <div className='col-12 col-md-3 col-lg-3 pt-4'>
+        <div className='col-12 col-md-4  pt-4'>
           <div className='text-center'>
         
           <a href="/" className="">
@@ -16,39 +16,29 @@ const Footer = () => {
           <div><p> Discover Earths treasures with our Geographic Navigator, guiding you through diverse landscapes, cultures, and wonders.   </p></div>
           </div>
           </div>
-        <div className='col-12 col-md-3 col-lg-3 pt-4'>
+        <div className='col-12 col-md-4  pt-4'>
         <p className="h5 " style={{ fontWeight: '600' }}>
          
           Contact
           </p>
           <div style={{ cursor: 'pointer', padding: '0' }}>
-            <div><a href=""> Resources  </a></div>
-            <div><a href=""> About Us  </a></div>
-            <div><a href=""> Contact  </a></div>
-            <div><a href=""> Blog  </a></div>
+         
+            <div><a href=""> +8801798740592  </a></div>
+            <div><a href=""> bdbengaigi@gmail.com  </a></div>
           </div>
         </div>
-        <div className='col-12 col-md-3 col-lg-3 pt-4'>
+        <div className='col-12 col-md-4  pt-4'>
           <p className="h5 " style={{ fontWeight: '600' }}>
-          Privacy Policy
+         Others
           </p>
           <div style={{ cursor: 'pointer', padding: '0' }}>
-            <div> <a href="">Support</a></div>
-            <div> <a href="">Customers</a></div>
-            <div> <a href="">Careers</a> </div>
+            <div> <a href="">About Us</a></div>
+            <div> <a href=""> Privacy Policy</a></div>
+            <div> <a href="">Terms & Condition</a></div>
+           
           </div>
         </div>
-        <div className='col-12 col-md-3 col-lg-3 pt-4'>
-          <p className="h5 " style={{ fontWeight: '600' }}>
-          Licensing
-          </p>
-          <div style={{ cursor: 'pointer', padding: '0' }}>
-        
-            <div><a href="">Windframe</a></div>
-            <div><a href="">Loop</a></div>
-            <div><a href="">Contrast</a></div>
-          </div>
-        </div>
+       
       </div>
       <div className="text-center mt-5">&copy; BengalGI , 2024. All rights reserved.</div>
       </div>
