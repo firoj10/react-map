f722542f27971cfd9db43cec369a726b9db1bedb
mrsharifull <116838065+mrsharifull@users.noreply.github.com>
2024-04-22 17:48:39 +0600

    logo section update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logo from './../../public/logo11.png'
 
 import "./map.css";
@@ -7,7 +8,10 @@ function Header() {
       <div className="card" style={{ backgroundColor: "#fff", border: 'none', borderRadius: '0px' }}>
         <div className="" style={{ alignItems: 'center' }}>
           <div className="" style={{ alignItems: 'center' }}>
+            
+            <Link to='/' >
             <img src={logo} className='logo' alt=""  />
+      </Link>
           </div>
          
         </div>
